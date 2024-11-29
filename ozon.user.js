f2b27a508e73287d4fbf94207318a7ed5cc5185b
TheSavlad <thesavlad@gmail.com>
2024-11-30 07:31:38 +0900

    Add SHA256 SRI to ozon.user.js

--- ozon.user.js
+++ ozon.user.js
@@ -7,8 +7,8 @@
 // @match        https://www.ozon.ru/my/orderlist*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon.ru
 // @grant        none
-// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/general.min.js
-// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/orderlist.min.js
+// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/general.min.js#sha256=4a0f6d017e4cd91185f80cd1c2cd297d886aaa4babdd5384e007e267b108304f
+// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/orderlist.min.js#sha256=7bbfde4a091c14017c070778990cd7289a446253a606bd10a98429d52230b05b
 // ==/UserScript==
 
 (function () {
