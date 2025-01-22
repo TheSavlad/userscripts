a5f5d5f5bed94036f4006f7573fab2010e128fcd
TheSavlad <thesavlad@gmail.com>
2025-01-22 17:21:32 +0900

    Update ozon.user.js 1.5

--- ozon.user.js
+++ ozon.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Ozon Helper
 // @namespace    https://www.ozon.ru
-// @version      1.4
+// @version      1.5
 // @description  Set of tweaks to Ozon website
 // @author       Savlad
 // @match        https://www.ozon.ru/my/orderlist*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon.ru
 // @grant        none
 // @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/general.min.js#sha256=4a0f6d017e4cd91185f80cd1c2cd297d886aaa4babdd5384e007e267b108304f
-// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/orderlist.min.js#sha256=1714b7329d159fbcef087d6f50a810e5072c7cfaf30b186f4cfbb1536f308103
+// @require      https://raw.githubusercontent.com/TheSavlad/userscripts/refs/heads/main/ozon/orderlist.min.js#sha256=45ec62ed060436e26580ca9c7d2b25a359b72fb27a0e62b216966933dc888e00
 // ==/UserScript==
 
 (function () {
